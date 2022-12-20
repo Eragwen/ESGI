d50dd829906a9d82add43881ea3ee2149f857a43
Eragwen <gwendal_besnard@hotmail.com>
2022-12-20 17:45:53 +0100

    tp somme & factoriel

--- Python/Py_TP1/main.py
+++ Python/Py_TP1/main.py
@@ -34,7 +34,35 @@ def calculSurface():
     plt.show()
 
 def sommeFactoriel():
-    num = int(input("entrer un entier positif : "))
+    sum = 0
+    fact = 1
+    while True:
+        num = int(input("entrer un entier positif : "))
+        if num < 1:
+            print(f"Vous avez choisi un entier non positif")
+            break
+        else :
+            for i in range(num):
+                if i >= 1:
+                    print(f"+ ", end='')
+                sum += i+1
+                print(f"{i+1} ", end='')
+            print(f"= {sum}")
+            for i in range(num):
+                if i >= 1:
+                    print(f"* ", end='')
+                fact *= i+1
+                print(f"{i+1} ", end='')
+            print(f"= {fact}")
+            replay = input("Voulez-vous recommencer [oO] ? ")
+
+            if replay.lower() != 'o':
+                print(f"Merci a bientot")
+                break
+            elif replay.upper() != 'O':
+                print(f"Merci a bientot")
+                break
+                
 
 def arbreNoel():
     hauteur = int(input("Hauteur de l'arbre : "))
@@ -61,5 +89,12 @@ def maths():
     print(f"Le sinus de {num} est {sin}")
     print(f"Le cosinus de {num} est {cos}")
 
+def tierce():
+    nb_partants = int(input("Combien y a-t-il de chevaux partants : "))
+    nb_joues = int(input("Combien y a-t-il de chevaux joues : "))
+
+    X = nb_partants /(nb_partants-nb_joues)
+
+
 if __name__ == "__main__":
-    maths()
+    sommeFactoriel()
